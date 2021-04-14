6a00220bcbfdd83a5e819e76af55cbc4f32d6b4e
Adrasteia <bjinmei@gmail.com>
2021-04-14 21:29:22 +0800

    Create simple_evolution_strategy.py

---
+++ simple_evolution_strategy.py
@@ -0,0 +1,27 @@
+import random
+
+POP_SIZA = 1000
+
+def random_pack():
+    return
+  
+def initialize():
+    return
+  
+def select():
+    return
+  
+def adjust():
+    return
+  
+def insert():
+    return
+  
+def search():
+    return
+  
+def merge():
+    return
+  
+def main():
+    return
